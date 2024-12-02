169b92f2d721ceb2c1f0c9733b5bf3a277a572ff
jinubee12 <jinubee12@naver.com>
2024-12-02 10:21:32 +0900

    new

--- likealion/Assets/Script/DataStructComp.cs
+++ likealion/Assets/Script/DataStructComp.cs
@@ -1,13 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class DataStructComp : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
+        Int32 intValue = 0; // 자료형 -21억 ~ 21억
+        UInt32 uintValue = 0; // 0~42억
         
+        Int64 longValue = 0; //
     }
 
     // Update is called once per frame
